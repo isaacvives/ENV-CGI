f47c1115c4f6629c8d8b3ea4617582a6ea54fd44
isaacvives <isaac.venus@student.vives.be>
2024-10-14 09:27:19 +0200

    Vervang harcoded username in setup_acl.sh door $USER

--- setup_acl.sh
+++ setup_acl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install acl
-setfacl -Rm u:pi:rwx,d:u:pi:rwx /var/www/html
-setfacl -Rm u:pi:rwx,d:u:pi:rwx /usr/lib/cgi-bin
+setfacl -Rm u:$USER:rwx,d:u:$USER:rwx /var/www/html
+setfacl -Rm u:$USER:rwx,d:u:$USER:rwx /usr/lib/cgi-bin
